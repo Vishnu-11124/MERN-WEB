5da7b3f7d963b1d210dc76dbdf3386abf9fd8367
vishnu-11124 <vishnusanthosh11124@gmail.com>
2025-09-24 16:39:32 +0530

    Started learning Tailwind CSS

---
+++ Tailwind-CSS/Basics/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold mx-4 my-3 text-blue-500">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
