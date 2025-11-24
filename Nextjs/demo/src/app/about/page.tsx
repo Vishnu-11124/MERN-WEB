bcdb0bf8dd4c60da245aecae5559b07972f0bbc2
vishnu-11124 <vishnusanthosh11124@gmail.com>
2025-11-24 16:31:46 +0530

    Started NextJs.

---
+++ Nextjs/demo/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-center my-6'>About</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ Nextjs/demo/src/app/products/mobiles/ios/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ios = () => {
+  return (
+    <div>
+      <h1>Ios mobiles are available</h1>
+    </div>
+  )
+}
+
+export default Ios
