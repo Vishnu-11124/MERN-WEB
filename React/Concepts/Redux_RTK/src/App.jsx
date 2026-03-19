0ada2a52dfe0e6d1e22813d40ef97b3e6f1577d2
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-03-19 21:47:26 +0530

    feat: initialize Redux RTK project with counter functionality
    - Added package.json for project dependencies and scripts
    - Created main application component (App.jsx) to render Counter
    - Implemented counter slice with increment and decrement actions
    - Configured Redux store to manage counter state
    - Developed Counter component to display and modify counter value
    - Set up main entry point (main.jsx) with Redux Provider
    - Added Vite configuration for project build
    - Included empty CSS file for styling

--- React/Concepts/Redux_RTK/src/App.jsx
+++ React/Concepts/Redux_RTK/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Counter from './components/Counter'
 
-
 const App = () => {
   return (
     <div>
-      <h1>Hello Boy</h1>
-      <Counter/>
+      <Counter />
     </div>
   )
 }
---
+++ React/Concepts/Redux_RTK/src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from '../app/features/counterSlice.js'
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
